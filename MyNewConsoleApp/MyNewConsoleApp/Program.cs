9cdb9c6d21e10eddd7b2c85c47e4f6eb7158d709
NAVAN <contactnavaneeth@gmail.com>
2021-09-09 17:45:15 +1000

    My First commit

---
+++ MyNewConsoleApp/MyNewConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyNewConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            bool y = false;
+            if (y==true)
+            {
+
+            }
+        }
+
+        //TODO
+        private void MyMethod()
+        {
+
+        }
+    }
+}
